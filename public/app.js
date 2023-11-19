5ad848d52bfb7834e6ef3e296657be7a517486bf
abdul-rahman-04 <151392538+abdul-rahman-04@users.noreply.github.com>
2023-11-19 18:54:56 +0530

    Update README.md

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   // Add an event listener for the "Search Routes" button
   const searchButton = document.querySelector('button');
-  searchButton.addEventListener('click', searchRoutes);
+  searchButton.addEventListener('click', redirectPage);
 });
 
 async function populateDropdown(dropdownId) {
@@ -17,7 +17,7 @@ async function populateDropdown(dropdownId) {
   const dropdown = document.getElementById(dropdownId);
   sourceDestinations.forEach(({ source, destination }) => {
     const option = document.createElement('option');
-    if(dropdownId === 'source') {
+    if (dropdownId === 'source') {
       option.value = `${source}`;
       option.text = `${source}`;
     } else {
@@ -28,38 +28,12 @@ async function populateDropdown(dropdownId) {
   });
 }
 
-async function searchRoutes() {
+async function redirectPage() {
   // Get the selected source, destination, and travel date
   const source = document.getElementById('source').value;
   const destination = document.getElementById('destination').value;
   const travelDate = document.getElementById('travel-date').value;
 
-  // Combine source and destination to send as a single parameter
-  const sourceDestination = `${source}-${destination}`;
-
-  // Make a request to the server to get the schedule
-  const scheduleResponse = await fetch(`/api/schedule/${sourceDestination}?date=${travelDate}`);
-  const schedule = await scheduleResponse.json();
-
-  // Display the schedule results
-  displayScheduleResults(schedule);
-}
-
-function displayScheduleResults(schedule) {
-  const scheduleResultsContainer = document.getElementById('schedule-results');
-  scheduleResultsContainer.innerHTML = '';
-
-  if(schedule.error === 'Schedule not found for the provided source-destination.') {
-    scheduleResultsContainer.innerHTML = '<p>Oops! No buses found.</p>';
-  } else if (schedule.length === 0) {
-    scheduleResultsContainer.innerHTML = '<p>No available schedule for the selected date.</p>';
-  } else {
-    const scheduleList = document.createElement('ul');
-    schedule.forEach(entry => {
-      const listItem = document.createElement('li');
-      listItem.textContent = `Date: ${entry.date}, Departure Time: ${entry.departureTime}, Arrival Time: ${entry.arrivalTime}, Duration: ${entry.duration}, Price: ${entry.price}, Seats Available: ${entry.seatsAvailable}`;
-      scheduleList.appendChild(listItem);
-    });
-    scheduleResultsContainer.appendChild(scheduleList);
-  }
+  // Redirect to the new page with query parameters
+  window.location.href = `./src/searchResults.html?source=${source}&destination=${destination}&travelDate=${travelDate}`;
 }
